4dfaa0295df1c6183e2e4b470f49c9fce1f78a67
David Lawlor <collegeitt2013@gmail.com>
2016-12-16 13:59:26 +0000

    finished data gathering

--- runtest.sh
+++ runtest.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+echo "CO	N	idle" > results.dat
 
-for i in {1..2}
+for i in {1..50}
 do
 	echo "Starting Load Test $i"
+
+	# Running the load test
 	./loadtest $i &
+
+	# echo the pid of the load test out to a file
 	echo $! > ./loadtest.pid
 
-	mpstat 2 5 | awk '/Average/ { print $NF }'
+	# save the output in a variable 
+	# 5 reports at 2 second intervals
+	# Average is awk'd and the last column of the line
+	Ucpu=`mpstat 2 5 | awk '/Average/ { print $NF }'`
 
-	kill `cat ./loadtest.pid`
+	# kill the load test process
+	kill `cat ./loadtest.pid` 
+
+	# count the number of completed processes
+	CO=`wc -l < synthetic.dat`
+
+	# output the load test results to file
+	echo "$i	$CO	$Ucpu" >> results.dat
 done
+
 
 echo "Finished"
